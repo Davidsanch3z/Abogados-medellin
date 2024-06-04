5f1349c62ddb5e4114000dbcbddf61f21f83a1da
Your Name <david@LAPTOP-6MIBVE8K>
2024-06-04 16:22:14 -0500

    nuevos cambios login fail

--- app.py
+++ app.py
@@ -1,48 +1,69 @@
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from flask import Flask, render_template, request, send_from_directory
+from werkzeug.utils import secure_filename 
 import os
 
+# Declarando nombre de la aplicación e inicializando
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads/'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Límite de tamaño de archivo de 16MB
 
+# Configuración del directorio de subida y descarga
+UPLOAD_FOLDER = 'static/archivos'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
+# Redireccionando cuando la página no existe
+@app.errorhandler(404)
+def not_found(error):
+    return 'Ruta no encontrada'
 
-@app.route('/contact')
+# Creando un Decorador
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    search_query = request.args.get('search', '')  # Obtener el término de búsqueda de los parámetros de la URL
+    archivos = os.listdir(app.config['UPLOAD_FOLDER'])
+
+    if search_query:
+        archivos = [archivo for archivo in archivos if search_query.lower() in archivo.lower()]
+
+    return render_template('index.html', archivos=archivos, search_query=search_query)
+
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
     return render_template('contact.html')
 
-@app.route('/index')
-def home():
-    return render_template('index.html')
+@app.route('/inicio', methods=['GET', 'POST'])
+def inicio():
+    return render_template('inicio.html')
 
-@app.route('/TIC')
+@app.route('/TIC', methods=['GET', 'POST'])
 def tic():
     return render_template('TIC.html')
 
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(request.url)
-    if file:
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('uploaded_file', filename=filename))
+@app.route('/registrar-archivo', methods=['GET', 'POST'])
+def registarArchivo():
+    archivo_subido = None
+    if request.method == 'POST':
+        # Script para archivo
+        file = request.files['archivo']
+        basepath = os.path.dirname(__file__) # La ruta donde se encuentra el archivo actual
+        filename = secure_filename(file.filename) # Nombre original del archivo
+        
+        upload_path = os.path.join(basepath, app.config['UPLOAD_FOLDER'], filename) 
+        file.save(upload_path)
+        archivo_subido = filename
+        
+    archivos = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', archivos=archivos, archivo_subido=archivo_subido)
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+# Nueva ruta para descargar archivos
+@app.route('/descargar-archivo/<filename>', methods=['GET'])
+def descargarArchivo(filename):
+    try:
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+    except FileNotFoundError:
+        return 'Archivo no encontrado', 404
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
-    
-
+    app.run(debug=True, port=5000)
